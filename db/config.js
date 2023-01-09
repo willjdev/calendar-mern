04c0fa3a4feb669dd016af20d01b7cfa2b59437e
William <william.jaimesfontalvo@gmail.com>
2023-01-08 22:45:53 -0500

    Backend 1.5

--- db/config.js
+++ db/config.js
@@ -1,27 +1,19 @@
 const mongoose = require('mongoose');
 
-
 const dbConnection = async() => {
 
     try {
+
+        mongoose.connect(process.env.DB_CNN);
+
+        console.log('DB Online')
         
-        await mongoose.connect( process.env.DB_CNN , {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-
-        console.log('DB Online');
-
-
     } catch (error) {
         console.log(error);
-        throw new Error('Error a la hora de inicializar BD');
+        throw new Error('Error al inicializar DB')
     }
 
-
 }
-
 
 module.exports = {
     dbConnection
