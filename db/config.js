92d8e2f686a3d79cc61df00315cafaeedcf4b387
William <william.jaimesfontalvo@gmail.com>
2023-01-08 22:32:14 -0500

    Backend 1.1

--- db/config.js
+++ db/config.js
@@ -4,7 +4,11 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.DB_CNN );
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
 
         console.log('DB Online')
         
